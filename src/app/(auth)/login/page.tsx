d165ffed3000c763237587541e74eececd64a1ac
ozzkan-burak <brk.ozzkan@gmail.com>
2025-05-01 17:15:24 +0300

    episode15

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+'use client';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolver/zod';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
   return <div>Login</div>;
