e67e6b37c5942145ee62625588efeae1202067de
ozzkan-burak <brk.ozzkan@gmail.com>
2025-05-08 23:34:17 +0300

    feat: episode16

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+'use client';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+  email: z.string().min(2, {
+    message: 'Email must be at least 2 characters.',
+  }),
+  password: z.string().min(2, {
+    message: 'Password must be at least 2 characters.',
+  }),
+});
+
+const onSubmit = async (data: z.infer<typeof formSchema>) => {
+  console.log(data);
+};
 const Register = () => {
-  return <div>Register Page</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-4/7">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="email@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="password" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default Register;
